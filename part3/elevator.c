f38450f4601bd66192f69381d0e7dfc00adcf94d
Spencer Lewis <srl16@my.fsu.edu>
2020-03-04 01:14:06 -0500

    Implementation of basic syscalls. Should run on compiled kernel

--- part3/elevator.c
+++ part3/elevator.c
@@ -11,6 +11,7 @@
 #include <linux/uaccess.h>
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
+#include <linux/mutex.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -20,8 +21,11 @@ int count = 0;
 int value = 0;
 static struct proc_dir_entry* proc_entry2;
 
+int currentfloor;
 struct task_struct * proc_entry;
 struct list_head passenger_list;
+struct mutex my_mutex;
+
 
 struct { int animal; int total_size; int destination; int type; int total_weight; struct list_head list; } elevator;
 typedef struct passengers {  int destination; int animal; int weight;  struct list_head list; } Passengers;
@@ -30,11 +34,11 @@ typedef struct floors { int start; int destination; int animal; int weight;  str
 int elevator_on(void * data);
 int add_passenger(Floors *b , struct list_head * position, int onfloor);
 int delete_passenger(struct list_head* position, Passengers *a, int ev);
-int request(Floors * myF, int rStart, int rDest, int rAnimal, int rWeight);
+int request(int rStart, int rDest, int rAnimal, int rWeight);
 
 
 int elevator_on(void * data) {
-	int currentfloor = 1;
+	currentfloor = 1;
 	Passengers *a;
 	Floors *b;
 
@@ -43,50 +47,21 @@ int elevator_on(void * data) {
 	struct list_head *position;
 	struct list_head *dummy;
 	printk(KERN_INFO "bob");
-	int pack =0;
-	// Declaration for adding a request
 
-	/*
-	Floors* people1 = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	if (people1 == NULL)
-		return -ENOMEM;
-	people1->start = 3;
-	people1->destination = 5;
-	people1->animal = 1;
-	people1->weight = 1;
-	list_add_tail(&people1->list, &passenger_list);
-	 people1 = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	 people1->start = 3;
-	 people1->destination = 7;
-	 people1->animal = 1;
-	 people1->weight = 1;
-	 list_add_tail(&people1->list, &passenger_list);
-	 people1 = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	 people1->start = 8;
-	 people1->destination = 2;
-	 people1->animal = 1;
-	 people1->weight = 1;
-	 list_add_tail(&people1->list, &passenger_list);
+/*
+	 request(people_one, 3, 5, 1, 1);
+	 request(people_one, 3, 7, 1, 1);
+	 request(people_one, 8, 2, 1, 1);
 */
 
-
-	 Floors* people_one = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	 request(people_one, 3, 5, 1, 1);
-	 people_one = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	 request(people_one, 3, 7, 1, 1);
-	 people_one = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
-	 request(people_one, 8, 2, 1, 1);
-
-
 	while (!kthread_should_stop()) {
-		pack = 0;
 		ssleep(1);
 		printk("me");
 		a = list_first_entry_or_null(&elevator.list, Passengers, list);
 		b = list_first_entry_or_null(&passenger_list, Floors, list);
 		if (elevator.total_size != 0 && elevator.destination == 0 && a != NULL) {
 			elevator.destination = a->destination;
-			printk(KERN_INFO " elvator people %d", elevator.destination);
+			printk(KERN_INFO " elvator people %d", a->animal);
 
 		}
 		else if (elevator.destination == 0 && elevator.total_size == 0 && b != NULL) {
@@ -97,11 +72,9 @@ int elevator_on(void * data) {
 
 
 		// Place Lock
-		printk(KERN_INFO " %d", elevator.total_size);
 		printk(KERN_INFO " %d" ,elevator.destination);
 		printk(KERN_INFO " %d", currentfloor);
 		if (elevator.destination != 0) {
-
 			list_for_each_safe(position,dummy ,&elevator.list) {
 				a = list_entry(position, Passengers, list);
 				if (a->destination == currentfloor) {
@@ -112,17 +85,12 @@ int elevator_on(void * data) {
 			}
 
 		// Place Lock
-			if (elevator.destination != -1) {
-				list_for_each_safe(position, dummy, &passenger_list) {
-					b = list_entry(position, Floors, list);
-					if (b->start == currentfloor) {
-						pack = add_passenger(b, position, currentfloor);
-						if (pack == 1) {
-							value = 1;
-						}
-					}
-					printk("me8");
+			list_for_each_safe(position,dummy ,&passenger_list) {
+				b = list_entry(position, Floors, list);
+				if (b->start == currentfloor) {
+					value = add_passenger(b,position,currentfloor);
 				}
+				printk("me8");
 			}
 			printk("me9");
 		}
@@ -170,11 +138,10 @@ int elevator_on(void * data) {
 int add_passenger(Floors* b, struct list_head * position,int onfloor) {
 	Passengers *people;
 	if(elevator.total_size ==0 ){}
-	else if (((elevator.total_weight + b->weight )> MAX_Weight) || (elevator.type != b->animal&& elevator.animal != 0) || (onfloor < elevator.destination && b->start > b->destination) || (onfloor > elevator.destination && b->start < b->destination)) {
-		printk("me2");
+	else if (elevator.total_weight + b->weight > MAX_Weight || (elevator.type == elevator.animal && elevator.animal != 0) || (onfloor < elevator.destination && b->start > b->destination) || (onfloor > elevator.destination && b->start < b->destination)) {
 		return 0;
 	}
-	
+	printk("me2");
 	people = kmalloc(sizeof(Passengers) * 1, __GFP_RECLAIM);
 	printk("me3");
 	if (people == NULL)
@@ -215,8 +182,13 @@ static struct file_operations myops = {
 };
 
 static int __init elevatorProtacal(void){
+
+	STUB_start_elevator = start_elevator;
+	STUB_stop_elevator = stop_elevator;
+	STUB_issue_request = issue_request;
+
 	proc_entry2 = proc_create("elevate", 0666, NULL, &myops);
-	 proc_entry  = kthread_run(elevator_on, NULL, "elevator");;
+	proc_entry  = kthread_run(elevator_on, NULL, "elevator");;
 
 	if(proc_entry == NULL)
 		return -ENOMEM;
@@ -225,8 +197,8 @@ static int __init elevatorProtacal(void){
 }
 
 
-int request(Floors * myF, int rStart, int rDest, int rAnimal, int rWeight) {
-
+int request(int rStart, int rDest, int rAnimal, int rWeight) {
+	Floors * myF = kmalloc(sizeof(Floors) * 1, __GFP_RECLAIM);
 	if (myF == NULL)
 		return -ENOMEM;
 	if (rStart > 10 || rStart < 1 || rDest > 10 || rDest < 1)
@@ -240,6 +212,24 @@ int request(Floors * myF, int rStart, int rDest, int rAnimal, int rWeight) {
 	return 1;
 }
 
+extern init (*STUB_start_elevator) (void);
+int start_elevator(void) {
+	currentfloor = 1;
+	return 0;
+}
+
+extern init (*STUB_stop_elevator) (void);
+int stop_elevator(void) {
+	elvator.destination = -1;
+	return 0;
+}
+
+extern init (*STUB_issue_request) (int passenger_type, int start_floor, int destination_floor);
+int issue_request(void) {
+	// I DO NOT THINK THIS IS CORRECT!!!!!!!!!!!!!!!!!
+	// not sure what to do with the animal variable here
+	request(start_floor, destination_floor, passenger_type/2, passenger_type);
+}
 
 static void __exit hello_end(void){
 
