c9361efb2e495a20efa021310764700181dcda4d
Coceus <46695012+Coceus@users.noreply.github.com>
2020-03-04 22:05:33 -0500

    comments

--- part1/part1.c
+++ part1/part1.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-#define __NR_part1_CALL1 323
-#define __NR_part1_CALL2 324
-#define __NR_part1_CALL3  325
-#define __NR_part1_CALL4  326
-#define __NR_part1_CALL5  327
+#define __NR_part1_CALL1 323			//system call number
+#define __NR_part1_CALL2 324			//system call number
+#define __NR_part1_CALL3  325			//system call number
+#define __NR_part1_CALL4  326			//system call number
+#define __NR_part1_CALL5  327           //system call number
 int main() {
-	syscall(__NR_part1_CALL1, 1);
-	syscall(__NR_part1_CALL2, 2);
-	syscall(__NR_part1_CALL3, 4);
-	syscall(__NR_part1_CALL4, 5);
-	syscall(__NR_part1_CALL5, 6);
+	syscall(__NR_part1_CALL1, 1);       //calling specific call at number
+	syscall(__NR_part1_CALL2, 2);		//calling specific call at number
+	syscall(__NR_part1_CALL3, 4);		//calling specific call at number
+	syscall(__NR_part1_CALL4, 5);		//calling specific call at number
+	syscall(__NR_part1_CALL5, 6);		//calling specific call at number
 
 
 }
